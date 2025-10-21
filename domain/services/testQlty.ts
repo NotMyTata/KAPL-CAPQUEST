3e4e3d4ce32cecf18124c0582d45245e8cc30002
NotMyTata <161783398+NotMyTata@users.noreply.github.com>
2025-10-21 11:16:01 +0700

    Simplify testQlty function implementation
    Refactored testQlty function to simplify logic.

--- domain/services/testQlty.ts
+++ domain/services/testQlty.ts
@@ -1,13 +1,1 @@
-export function testQlty(a: number): number{
-    let total = 0;
-    if(a > 0){
-        for(a; a > -1; a+=2){
-            total += a;
-        }
-    } else {
-        for(a; a < 1; a--){
-            total += a;
-        }
-    }
-    return total;
-}
+export function testQlty(a: number): number{ let total = 0; for(a; a > -1; a+=2) { total += a; } return total; }
