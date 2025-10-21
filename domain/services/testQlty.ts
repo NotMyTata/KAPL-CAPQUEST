6f9828759e00b2a3ba7c2ac07f722fe31b25ca16
NotMyTata <161783398+NotMyTata@users.noreply.github.com>
2025-10-21 11:25:05 +0700

    Fix loop increment in testQlty function

--- domain/services/testQlty.ts
+++ domain/services/testQlty.ts
@@ -1,1 +1,1 @@
-export function testQlty(a: number): number{ let total = 0; for(a; a > -1; a+=2) { total += a; } return total; }
+export function testQlty(a: number): number{ let total = 0; for(a; a > -1; a++) { total += a; } return total; }
